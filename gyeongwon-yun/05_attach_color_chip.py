3f75636e639daf451ce5e4045fd52b4c799b60d7
gwyun <gyeongwonyun@gmail.com>
2023-04-13 18:36:48 +0900

    0411 study 코드 정리

--- gyeongwon-yun/05_attach_color_chip.py
+++ gyeongwon-yun/05_attach_color_chip.py
@@ -1,131 +1,88 @@
-import sys
-from copy import deepcopy
+##reference: https://brorica.tistory.com/entry/%EB%B0%B1%EC%A4%8017136-%EC%83%89%EC%A2%85%EC%9D%B4-%EB%B6%99%EC%9D%B4%EA%B8%B0-Python3
+import math
 
-def solution(board):
+sizes = [0, 0, 0, 0, 0, 0]
+visit = [[False for _ in range(10)] for _ in range(10)]
+N = 10
+ans = float("inf")
+board = []
+for _ in range(N):
+    board.append(list(map(int, input().split())))
+
+def main():
     ones = 0
-
-    for r in range(10):
-        for c in range(10):
+    for r in range(N):
+        for c in range(N):
             if board[r][c] == 1:
                 ones += 1
+    dfs(0,ones)
+    if ans == float("inf"):
+        print(-1)
+    else:
+        print(ans)
 
-    global cnt
-    cnt = 26
- 
-    
-    dfs(board, 0, 0, 0, 0, 0, 0, ones, 5)
-
-    return cnt if cnt > 0 else -1
-
-
-        
-        
-        
-
-def dfs(board, a, b, c, d, e, used, ones, limit):
-    global cnt
-
+def dfs(count, ones):
+    global ans
+    if count >= ans:
+        return
     if ones == 0:
-        cnt = min(cnt, used)
+        ans = min(ans, sum(sizes))
         return
-    
-    if used >= cnt:
-        return
-    
-    if limit > 4 and e < 5 and ones >= 5**2:
-        j = 5
-        for r in range(10):
-            for c in range(10):
-                if board[r][c] == 1 and can_paste_chip(board, j, r, c):
-                    new_board = deepcopy(board)
-                    for x in range(r, r+j):
-                        for y in range(c, c+j):
-                            new_board[x][y] = 0
-                    dfs(new_board, a, b, c, d, e+1, used+1, ones-j**2, j if e < 5 else j-1)
-    
-    if limit > 3 and d < 5 and ones >= 4**2:
-        j = 4
-        for r in range(10):
-            for c in range(10):
-                if board[r][c] == 1 and can_paste_chip(board, j, r, c):
-                    new_board = deepcopy(board)
-                    for x in range(r, r+j):
-                        for y in range(c, c+j):
-                            new_board[x][y] = 0
-                    dfs(new_board, a, b, c, d+1, e, used+1, ones-j**2, j if d < 5 else j-1)
-    if ones > sum(5*x**2 for x in [1, 2, 3]):
-        return
-    
-    if limit > 2 and b < 5 and ones >= 3**2:
-        j = 3
-        for r in range(10):
-            for c in range(10):
-                if board[r][c] == 1 and can_paste_chip(board, j, r, c):
-                    new_board = deepcopy(board)
-                    for x in range(r, r+j):
-                        for y in range(c, c+j):
-                            new_board[x][y] = 0
-                    dfs(new_board, a, b, c+1, d, e, used+1, ones-j**2, j if c < 5 else j-1)
-    if ones > sum(5*x**2 for x in [1, 2]):
-        return
-    
-    if limit > 1 and b < 5 and ones >= 2**2:
-        j = 2
-        for r in range(10):
-            for c in range(10):
-                if board[r][c] == 1 and can_paste_chip(board, j, r, c):
-                    new_board = deepcopy(board)
-                    for x in range(r, r+j):
-                        for y in range(c, c+j):
-                            new_board[x][y] = 0
-                    dfs(new_board, a, b+1, c, d, e, used+1, ones-j**2, j if b < 5 else j-1)
-    if ones > 5:
-        return
-    
-    if limit > 0 and a < 5 and ones >= 1**2:
-        j = 1
-        for r in range(10):
-            for c in range(10):
-                if board[r][c] == 1 and can_paste_chip(board, j, r, c):
-                    new_board = deepcopy(board)
-                    for x in range(r, r+j):
-                        for y in range(c, c+j):
-                            new_board[x][y] = 0
-                    dfs(new_board, a+1, b, c, d, e, used+1, ones-j**2, j if a < 5 else j-1)
-    
 
-def can_paste_chip(board, j, r, c):
-    if r+j > 10 or c+j > 10: # out of board
-        return False
-    
-    cropped_board = [row[c:c+j] for row in board[r:r+j]]
 
-    multiply = 1
-    for row in cropped_board:
-        for x in row:
-            multiply *= x
-    
-    if multiply == 1:
-        return True
-    
-    return False
+    for y in range(N):
+        for x in range(N):
+            if not board[y][x]:
+                continue
+            for s in range(4, -1, -1):
+                if sizes[s + 1] >= 5 or y + s >= N or x + s >= N or not check(y, x, s):
+                    continue
+                fill(y, x, s)
+                dfs(count + 1, ones-((s+1)**2))
+                restore(y, x, s)
+            return
 
-input = sys.stdin.readline
-board = [list(map(int, input().split())) for _ in range(10)]
+def fillCheck():
+    for y in range(N):
+        for x in range(N):
+            if board[y][x]:
+                return False
+    return True
 
-# print(*board, sep='\n')
-print(solution(board))
+def check(Y, X, s):
+    for y in range(Y, Y + s + 1):
+        for x in range(X, X + s + 1):
+            if not board[y][x]:
+                return False
+    return True
 
-#반례: 5
+def fill(Y, X, s):
+    for y in range(Y, Y + s + 1):
+        for x in range(X, X + s + 1):
+            board[y][x] = 0
+    sizes[s + 1] += 1
+
+def restore(Y, X, s):
+    for y in range(Y, Y + s + 1):
+        for x in range(X, X + s + 1):
+            board[y][x] = 1
+    sizes[s + 1] -= 1
+
+
+
+
+if __name__ == "__main__":
+    main()
+
 '''
-1 1 1 1 1 1 1 0 0 0
-1 1 1 1 1 1 1 0 0 0
-1 1 1 1 1 1 1 0 0 0
-1 1 1 1 1 1 1 0 0 0
-1 1 1 1 1 1 1 0 0 0
-1 1 1 1 1 0 0 0 0 0
-1 1 1 1 1 0 0 0 0 0
-1 1 1 1 1 0 0 0 0 0
-1 1 1 1 1 0 0 0 0 0
+0 0 0 0 1 1 1 1 1 1
+0 1 1 1 1 1 1 1 1 1
+0 1 1 1 1 1 1 1 1 1
+0 1 1 1 1 1 1 1 1 1
+0 1 1 1 1 1 1 1 1 1
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
 0 0 0 0 0 0 0 0 0 0
 '''
